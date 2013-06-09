f05de6efb19ced1df8e7dd7ad27ed3ecdd093fa4
Mark Wood <mark@markozy.co.uk>
2013-06-09 14:33:20 +0100

    Added Main.js, initiates rendering and attaches resulting HTML to the DOM.

---
+++ guitartab.js/scripts/main.js
@@ -0,0 +1,19 @@
+if (typeof (GuitarTab) === "undefined") var GuitarTab = {};
+
+require(['Renderer'], function(renderer){
+    if (GuitarTab.tab){
+        document.getElementById('tab-title').appendChild(document.createTextNode(GuitarTab.tab.title));
+        document.getElementById('tab-artist').appendChild(document.createTextNode(GuitarTab.tab.artist));
+
+        var drawingCanvas = document.getElementById('drawing-canvas');
+
+        // Convert to HTML and attach to the DOM
+        for (var partIndex = 0; partIndex < GuitarTab.tab.parts.length; partIndex++) {
+            var part = GuitarTab.tab.parts[partIndex];
+
+            for (var measureIndex = 0; measureIndex < GuitarTab.tab.lengthInMeasures; measureIndex++) {
+                drawingCanvas.appendChild(renderer.convertJsonMeasureToHtml(part.measures[measureIndex], part.numberOfStrings, measureIndex));
+            }
+        }
+    }
+});
